2e57f5f24f1c110d1d1bb9a9cbdc14b3c3cec9a0
Alexander Rogers <60371791+arogers325@users.noreply.github.com>
2020-04-19 15:46:04 -0500

    NextJS files

---
+++ myproject/pages/items/[itemID].js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
+    <section>
+        <h1>Item Details</h1>
+        <img src={props.item.image} />
+        <p>Description: {props.item.description}</p>
+        <p>Price: ${props.item.price}</p>
+    </section>
+);
+
+Index.getInitialProps = async function(context) {
+    const {itemId} = context.query;
+    const res = await fetch(`http://localhost:5555/items/${itemId}`);
+    const item = await res.json();
+
+    console.log(`Show data fetched. Item: ${item}`);
+
+    return {
+        item
+    };
+};
+
+export default Index;
